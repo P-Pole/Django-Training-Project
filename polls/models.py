652677c51456a80b5a140f328957ea76ba6805ff
--replace-all <mszczepanska@student.agh.edu.pl>
2023-02-08 19:46:37 +0100

    Checking if I can set everything up properly from a new machine

--- polls/models.py
+++ polls/models.py
@@ -10,3 +10,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerChoices(default=0)
+
+ #Test
